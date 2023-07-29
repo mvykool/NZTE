3ce7542e27d2ae9cb5693b436c438d9d465eacac
maicol hernandez <maykolandres4@gmail.com>
2023-07-29 14:39:52 -0500

    create one handler

--- node-zod/src/api/todos/todo.handler.ts
+++ node-zod/src/api/todos/todo.handler.ts
@@ -1,8 +1,23 @@
-import { Response, Request } from 'express';
-import { TodoWithId, Todos } from './todo.model';
+import { Response, Request, NextFunction } from 'express';
+import { Todo, TodoWithId, Todos } from './todo.model';
+import { InsertOneResult } from 'mongodb';
 
-export async function findAll(req: Request, res: Response<TodoWithId[]>) {
-  const result = await Todos.find();
-  const todos = await result.toArray();
-  res.json(todos); 
+export async function findAll(req: Request, res: Response<TodoWithId[]>, next: NextFunction) {
+  try {
+    const result = await Todos.find();
+    const todos = await result.toArray();
+    res.json(todos); 
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createOne(req: Request<{}, InsertOneResult<Todo>, Todo>, res: Response<InsertOneResult<Todo>>, next: NextFunction) {
+  try {
+    const validateResult = await Todo.parse(req.body);
+    const insertResult = await Todos.insertOne(validateResult);
+    res.json(insertResult);
+  } catch (error) {
+	  next(error);
+  }
 }
--- node-zod/src/api/todos/todo.route.ts
+++ node-zod/src/api/todos/todo.route.ts
@@ -4,5 +4,6 @@ import * as todoHandler from './todo.handler';
 const router = Router();
 
 router.get('/', todoHandler.findAll);
+router.post('/', todoHandler.createOne);
 
 export default router;
