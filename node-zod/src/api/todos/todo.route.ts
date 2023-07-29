676eddd86778528631c09b95cc37892d7580cc48
maicol hernandez <maykolandres4@gmail.com>
2023-07-29 13:43:51 -0500

    tried to connect to db

--- node-zod/src/api/todos/todo.route.ts
+++ node-zod/src/api/todos/todo.route.ts
@@ -1,13 +1,12 @@
 import { Router, Request, Response } from 'express';
-import Todo from './todo.model';
+import { TodoWithId, Todos } from './todo.model';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response<Todo[]>) => {
-  res.json([{
-    content: 'learn typescript',
-    done: false,
-  }]);
+router.get('/', async (req: Request, res: Response<TodoWithId[]>) => {
+  const result = await Todos.find();
+  const todos = await result.toArray();
+  res.json(todos); 
 });
 
 export default router;
